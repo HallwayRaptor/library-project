cae80e2a783fe928cc7c340bcad404dca467b449
rob <rob@robs-iMac-Pro.local>
2022-06-21 11:14:03 -0700

    add test book

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let myLibrary = [];
 
-function addBookToLibrary() {}
-
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -12,6 +10,13 @@ function Book(title, author, pages, read) {
 	};
 }
 
-const testBook = new Book("test book", "test author", 399, "unread");
+const title = prompt("Book Title");
+const author = prompt("Author");
+const pages = prompt("Pages");
+const read = prompt("is it read?");
 
-console.log(testBook.info());
+const book1 = new Book(title, author, pages, read);
+
+console.log(book.info());
+
+function addBookToLibrary() {}
